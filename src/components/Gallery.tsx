e9094700ae4efc45649ba38ce194e2d398205223
Iris <rislamb42@gmail.com>
2021-05-09 11:21:00 -0400

    Fade in images on load

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,25 +1,36 @@
 import { artList } from './allImages';
 import { Image } from 'antd';
 import { EyeFilled } from '@ant-design/icons';
-import "./Gallery.css";
+import './Gallery.css';
+import { useState } from 'react';
 
 export const Gallery = ({ arts }: { arts: artList }) => {
   return (
     <div className="gallery">
       <Image.PreviewGroup>
         {arts.map(({ name, ext }) => (
-          <Image
-            key={name}
-            width={400}
-            src={require(`./images/${name}-crop.${ext}`).default}
-            preview={{
-              src: require(`./images/${name}.${ext}`).default,
-              mask: <EyeFilled />
-            //   TODO: mask is not centered for soem reason
-            }}
-          />
+          <FadeInImage key={name} name={name} ext={ext} />
         ))}
       </Image.PreviewGroup>
+    </div>
+  );
+};
+
+const FadeInImage = ({ name, ext }: { name: string; ext: string }) => {
+  const [loaded, setLoaded] = useState(false);
+  return (
+    <div className="fade-in-image">
+      <Image
+        className={`img-${loaded ? 'loaded' : 'not-loaded'}`}
+        loading="lazy"
+        onLoad={() => setLoaded(true)}
+        width={400}
+        src={require(`./images/${name}-crop.${ext}`).default}
+        preview={{
+          src: require(`./images/${name}.${ext}`).default,
+          mask: <EyeFilled />,
+        }}
+      />
     </div>
   );
 };
