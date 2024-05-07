6d3a6dc9b5a4d1d6ac28a67d9b2a2618c0cddd3e
Iris <rislamb42@gmail.com>
2024-05-07 18:41:49 -0400

    Update portfolio

--- src/components/allImages.ts
+++ src/components/allImages.ts
@@ -2,28 +2,32 @@
 export type artList = {name: string, ext: string}[];
 
 export const illos: artList = [
-    {name: '22-9-19', ext: 'png'},
-    {name: '21-8-3', ext: 'png'},
-    {name: '21-5-25', ext: 'png'},
+    {name: '23-8-22', ext: 'png'}, // anaestha
+    {name: '22-12-10', ext: 'png'}, // riska
+    {name: 'con-22-6-26', ext: 'jpg'}, // scythe
 
-    {name: '20-4-27', ext: 'png'},
-    {name: '21-12-21', ext: 'png'},
-    {name: '21-3-6', ext: 'png'},
+    {name: '21-5-25', ext: 'png'}, // cyborg
+    {name: '21-8-3', ext: 'png'}, // enoch
+    {name: '22-9-19', ext: 'png'}, // kiriona
 
-    {name: '22-5-10', ext: 'png'},
-    {name: '21-4-23', ext: 'png'},
-    {name: '19-7-17', ext: 'jpg'},
+    {name: '21-12-21', ext: 'png'}, // whale
+    {name: '22-5-10', ext: 'png'}, // deranged
+    {name: '21-3-6', ext: 'png'}, // dream
 
-    {name: '20-9-4', ext: 'jpg'},
-    {name: '19-5-16', ext: 'png'},
-    {name: '20-6-12', ext: 'png'},
-    // {name: '20-8-1', ext: 'png'},
+    {name: '20-9-4', ext: 'jpg'}, // nikaido
+    {name: '19-7-17', ext: 'jpg'}, // gomens
+    {name: '21-4-23', ext: 'png'}, // feathers
+
+    {name: '19-5-16', ext: 'png'}, // comic
 ];
 
 export const concepts: artList = [
     {name: 'con-20-12-20', ext: 'png'},
+    {name: 'con-22-6-26', ext: 'jpg'},
     {name: 'con-21-5-2', ext: 'png'},
+    {name: 'con-23-2-25', ext: 'png'},
     {name: 'con-21-4-30', ext: 'png'},
-    {name: 'con-19-4-25', ext: 'jpg'},
     {name: 'con-22-8-7', ext: 'png'},
+    {name: 'con-19-4-25', ext: 'jpg'},
+
 ];
