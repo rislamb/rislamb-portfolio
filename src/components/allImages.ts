edcf9d7727433f763c6f84548c58d2204e551adc
Iris <rislamb42@gmail.com>
2025-01-08 21:41:35 -0500

    Update images

--- src/components/allImages.ts
+++ src/components/allImages.ts
@@ -4,20 +4,20 @@ export type artList = {name: string, ext: string}[];
 export const illos: artList = [
     {name: '23-8-22', ext: 'png'}, // anaestha
     {name: '22-12-10', ext: 'png'}, // riska
-    {name: 'con-22-6-26', ext: 'jpg'}, // scythe
+    {name: '24-8-4', ext: 'png'}, // scythe
 
+    {name: '24-10-13', ext: 'png'}, // enoch + sig
     {name: '21-5-25', ext: 'png'}, // cyborg
     {name: '21-8-3', ext: 'png'}, // enoch
-    {name: '22-9-19', ext: 'png'}, // kiriona
 
+    {name: '22-9-19', ext: 'png'}, // kiriona
     {name: '21-12-21', ext: 'png'}, // whale
     {name: '22-5-10', ext: 'png'}, // deranged
-    {name: '21-3-6', ext: 'png'}, // dream
 
-    {name: '20-9-4', ext: 'jpg'}, // nikaido
     {name: '19-7-17', ext: 'jpg'}, // gomens
+    {name: '20-9-4', ext: 'jpg'}, // nikaido
     {name: '21-4-23', ext: 'png'}, // feathers
-
+    
     {name: '19-5-16', ext: 'png'}, // comic
 ];
 
